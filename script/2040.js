99b8e379dc1bf201584b1b537de07ff3ce826886
arcangelo7 <arcangelomas@gmail.com>
2020-08-14 18:01:58 +0200

    glowing circuit

--- script/2040.js
+++ script/2040.js
@@ -1,5 +1,12 @@
 $(function(){
+    // Glitches
     $(".around_2040").find("h1, h2, h3, h4, h5, h6").each(function() {
         $(this).attr("data-text", $(this).text());
     });
+
+
+    // Tablet-like device
+    $(".around_2040").children().wrapAll("<main>");
 });
+
+
