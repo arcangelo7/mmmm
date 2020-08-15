2d0f4a8a8b5774d86be3c9d70105334e4cb6df68
arcangelo7 <arcangelomas@gmail.com>
2020-08-15 16:07:55 +0200

    clip path

--- script/2040.js
+++ script/2040.js
@@ -1,12 +1,12 @@
 $(function(){
     // Glitches
-    $(".around_2040").find("h1, h2, h3, h4, h5, h6").each(function() {
+    $(".around_2040").find("h1, h2, h3, h4, h5, h6, .nav__item a").each(function() {
         $(this).attr("data-text", $(this).text());
     });
 
 
     // Tablet-like device
-    $(".around_2040").children().wrapAll("<main>");
+    $(".around_2040").children(":not(nav, footer)").wrapAll("<main>");
 });
 
 
