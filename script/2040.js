7d6791e075891b2759e097d8af7dba6956b3258a
Arcangelo <info@arcangelomassari.com>
2020-08-28 13:12:39 +0200

    Medieval navbar for pc and tablet. No smartphone

--- script/2040.js
+++ script/2040.js
@@ -1,86 +1,114 @@
 $(function(){
-    // Glitches
-    $(".around_2040").find("h1, h2, h3, h4, h5, h6, .nav__item a").each(function() {
-        $(this).attr("data-text", $(this).text());
-    });
+    if ($(".around_2040").length > 0) {
+        // Add nav with eye
+        // $(".around_2040").prepend(`
+        //     <nav class="nav">
+        //         <ul class="nav__bar"> 
+        //             <li class="nav__item nav__item--logo visited"><a href="#">mmmm</a></li>
+        //             <div class="eye">
+        //                 <div class="blade"><div></div></div>  
+        //                 <div class="blade"><div></div></div>
+        //                 <div class="blade"><div></div></div>  
+        //                 <div class="blade"><div></div></div>
+        //                 <div class="blade"><div></div></div>  
+        //                 <div class="blade"><div></div></div>
+        //                 <div class="blade"><div></div></div>  
+        //                 <div class="blade"><div></div></div>
+        //                 <div class="iris"></div>
+        //             </div>
+        //             <li class="nav__item"><a href="#">nutella</a></li>
+        //         </ul>
+        //     </nav>
+        // `);
+        $(".nav__item--logo").after(`
+            <div class="eye">
+                <div class="blade"><div></div></div>  
+                <div class="blade"><div></div></div>
+                <div class="blade"><div></div></div>  
+                <div class="blade"><div></div></div>
+                <div class="blade"><div></div></div>  
+                <div class="blade"><div></div></div>
+                <div class="blade"><div></div></div>  
+                <div class="blade"><div></div></div>
+                <div class="iris"></div>
+            </div>
+        `);
 
-
-    // Tablet-like device
-    function wrapInMain(x) {
-        if (x.matches) { // If media query matches
-            $(".tablet").children().unwrap();
-            $(".around_2040").children(":not(nav)").wrapAll("<div class='container'>");
-            $(".container").children(":not(footer)").wrapAll("<div class='tablet'>");
-        } else {
-            $(".tablet").children().unwrap();
-            $(".container").children().unwrap();
-            $(".around_2040").children(":not(nav, footer)").wrapAll("<main class='tablet'>");
-        }
-    }
-      
-    var x = window.matchMedia("(max-width: 37.5em)")
-    wrapInMain(x) // Call listener function at run time
-    x.addListener(wrapInMain) // Attach listener function on state changes
-
-
-    // Scroll into view
-    const toFade = document.querySelectorAll('table, p, h1, h2, h3, h4, h5, h6, a');
-
-    // const config = {
-    //     rootMargin: '-100px'
-    //     // threshold: [0, 0.25, 0.75, 1]
-    // };
-
-    observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.intersectionRatio > 0) {
-                entry.target.classList.add('fade-in')
-                // observer.unobserve(entry.target);
-            } else {
-                entry.target.classList.remove('fade-in');
-            } 
+        // Glitches
+        $(".around_2040").find("h1, h2, h3, h4, h5, h6, .nav__item a").each(function() {
+            $(this).attr("data-text", $(this).text());
         });
-    });
-      
-    toFade.forEach(table => {
-        observer.observe(table);
-    });
 
-    // Eye tracking
-    // var $eye = $('.box');
-    // var eyeX = $eye.offset().left + $eye.width() / 2;
-    // var eyeY = $eye.offset().top + $eye.height() / 2;
-    // $('body').on('mousemove', function(event){
-    //   var rad = Math.atan2(event.pageY - eyeY, event.pageX - eyeX);
-    //   var rot = rad * (180 / Math.PI);
-    //   $eye.css('transform', 'rotate(' + rot + 'deg)' )
-    // })
 
-    $(document).mousemove(function(e){
-        var eye = $('.eye');
-        var eyeX = (eye.offset().left + eye.width() / 2);
-        var eyeY = (eye.offset().top + eye.height() / 2);
-        var eyeXPercent = Math.round(eyeX / $(this).width() * 100)
-        var eyeYPercent = Math.round(eyeY / $(this).width() * 100)
-        var xPos = e.pageX; 
-        var yPos = e.pageY;
-        var mouseXPercent = Math.round(xPos / $(this).width() * 100);
-        var mouseYPercent = Math.round(yPos / $(this).height() * 100);
-
-        $('.iris').css('top',mouseYPercent - eyeYPercent);
-        $('.iris').css('left',mouseXPercent - eyeXPercent);
-
-        var iris = $(".iris");
-        if (parseInt(iris.css("top")) > 8) {
-            iris.css("top", "8");
+        // Tablet-like device
+        function wrapInMain(x) {
+            if (x.matches) { // If media query matches
+                $(".tablet").children().unwrap();
+                $(".around_2040").children(":not(nav)").wrapAll("<div class='container'>");
+                $(".container").children(":not(footer)").wrapAll("<div class='tablet'>");
+            } else {
+                $(".tablet").children().unwrap();
+                $(".container").children().unwrap();
+                $(".around_2040").children(":not(nav, footer)").wrapAll("<main class='tablet'>");
+            }
         }
-        if (parseInt(iris.css("left")) < -8) {
-            iris.css("left", "-8");
-        }
-        if (parseInt(iris.css("left")) > 8) {
-            iris.css("left", "8");
-        }
-    }); 
+        
+        var x = window.matchMedia("(max-width: 37.5em)")
+        wrapInMain(x) // Call listener function at run time
+        x.addListener(wrapInMain) // Attach listener function on state changes
+
+
+        // Scroll into view
+        const toFade = document.querySelectorAll('table, p, h1, h2, h3, h4, h5, h6, a');
+
+        // const config = {
+        //     rootMargin: '-100px'
+        //     // threshold: [0, 0.25, 0.75, 1]
+        // };
+
+        observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.intersectionRatio > 0) {
+                    entry.target.classList.add('fade-in')
+                    // observer.unobserve(entry.target);
+                } else {
+                    entry.target.classList.remove('fade-in');
+                } 
+            });
+        });
+        
+        toFade.forEach(table => {
+            observer.observe(table);
+        });
+
+
+        // Eye tracking
+        $(document).mousemove(function(e){
+            var eye = $('.eye');
+            var eyeX = (eye.offset().left + eye.width() / 2);
+            var eyeY = (eye.offset().top + eye.height() / 2);
+            var eyeXPercent = Math.round(eyeX / $(this).width() * 100)
+            var eyeYPercent = Math.round(eyeY / $(this).width() * 100)
+            var xPos = e.pageX; 
+            var yPos = e.pageY;
+            var mouseXPercent = Math.round(xPos / $(this).width() * 100);
+            var mouseYPercent = Math.round(yPos / $(this).height() * 100);
+
+            $('.iris').css('top',mouseYPercent - eyeYPercent);
+            $('.iris').css('left',mouseXPercent - eyeXPercent);
+
+            var iris = $(".iris");
+            if (parseInt(iris.css("top")) > 8) {
+                iris.css("top", "8");
+            }
+            if (parseInt(iris.css("left")) < -6) {
+                iris.css("left", "-6");
+            }
+            if (parseInt(iris.css("left")) > 6) {
+                iris.css("left", "6");
+            }
+        }); 
+    }
 });
 
 
---
+++ script/general.js
@@ -0,0 +1,10 @@
+// No sep between biblioRef
+$(function(){
+    $("p").each(function(index){
+        var sepBetweenRef = new RegExp("</a>.{1}<a")
+        if($(this).html().match(sepBetweenRef)){
+            var found = $(this).html().match(sepBetweenRef)[0];
+            document.body.innerHTML = document.body.innerHTML.replace(found, "</a><a");
+        }
+    })
+});
